ccbbe23405860529dd2314fc172d93eecb2523f0
ANDRE <klai1@mail.ru>
2022-10-13 19:59:22 +0400

    Initial commit

---
+++ src/main/java/com/skypro/streamapioption/StreamApiOptionalApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.streamapioption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamApiOptionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamApiOptionalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/streamapioption/StreamApiOptionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.streamapioption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamApiOptionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
